49eece8e4e3296d4814db0cb2f23393941db3e5d
Pinheiro Yan <yan.pinheiro@volvo.com>
2023-11-29 22:40:03 -0300

    c

--- src/main/java/aula/web/adivinhe/ws/JogadorResource.java
+++ src/main/java/aula/web/adivinhe/ws/JogadorResource.java
@@ -1,11 +1,19 @@
 package aula.web.adivinhe.ws;
 
 import aulas.web.adivinhe.entity.Jogador;
+import aulas.web.adivinhe.entity.Jogo;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import java.util.List;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/jogador")
 public class JogadorResource {
@@ -26,6 +34,80 @@ public class JogadorResource {
         return jogadores;
     }
 
+    
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response createJogador(Jogador jogador) {
+        // Antes de persistir, você deve verificar se o jogador com o mesmo código já existe para evitar duplicatas
+        if (Jogador.findById(jogador.codigo) != null) {
+            return Response.status(Response.Status.CONFLICT).entity("Jogador com código " + jogador.codigo + " já existe.").build();
+        }
+
+        // Você pode adicionar a lógica de validação ou hash da senha aqui
+
+        Jogador.persist(jogador);
+        if (jogador.isPersistent()) {
+            return Response.status(Response.Status.CREATED).entity(jogador).build();
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+    
+    
+    @PUT
+    @Path("/update/{codigo}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response updateJogador(@PathParam("codigo") Integer codigo, Jogador jogadorAtualizado) {
+        Jogador jogadorExistente = Jogador.findById(codigo);
+
+        if (jogadorExistente == null) {
+            // Jogador com o código fornecido não encontrado.
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        // Atualize os campos do jogador existente com os valores do jogador atualizado.
+        jogadorExistente.apelido = jogadorAtualizado.apelido;
+        jogadorExistente.nome = jogadorAtualizado.nome;
+        jogadorExistente.email = jogadorAtualizado.email;
+        // Não atualize a senha ou outros campos sensíveis diretamente sem validar ou hash-los adequadamente.
+        jogadorExistente.dataNasc = jogadorAtualizado.dataNasc;
+        jogadorExistente.endereco = jogadorAtualizado.endereco;
+
+        // Persiste as alterações no banco de dados.
+        jogadorExistente.persist();
+
+        return Response.ok(jogadorExistente).build();
+    }
+    
+    
+    
+    @DELETE
+    @Path("/delete/{codigo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response deleteJogador(@PathParam("codigo") Integer codigo) {
+        Jogador jogador = Jogador.findById(codigo);
+        if (jogador == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        // Primeiro, exclui todos os jogos associados ao jogador
+        jogador.jogos.forEach(jogo -> jogo.delete());
+
+        // Agora, exclui o próprio jogador
+        jogador.delete();
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+    
+    
+    
+    
     private void verificaPermissao(Jogador jogador) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
--- src/main/java/aula/web/adivinhe/ws/JogoResource.java
+++ src/main/java/aula/web/adivinhe/ws/JogoResource.java
@@ -1,5 +1,6 @@
 package aula.web.adivinhe.ws;
 
+import aulas.web.adivinhe.entity.Jogador;
 import aulas.web.adivinhe.entity.Jogo;
 import aulas.web.adivinhe.entity.JogoPK;
 import java.net.URI;
@@ -9,9 +10,11 @@ import java.util.Date;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -55,4 +58,51 @@ public class JogoResource {
         String iso = DateTimeFormatter.ISO_INSTANT.format(instant);
         return Response.created(URI.create("/jogo/info/" + jogo.jogoPK.jogador + "/" + iso)).build();
     }
+    
+    
+    @DELETE
+    @Path("/{codigoJogador}/jogos/{idJogo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response deleteJogo(@PathParam("codigoJogador") Integer codigoJogador, @PathParam("idJogo") Long idJogo) {
+        Jogador jogador = Jogador.findById(codigoJogador);
+        if (jogador == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Jogador não encontrado.").build();
+        }
+
+        Jogo jogo = Jogo.findById(idJogo);
+        if (jogo == null || !jogo.jogador.equals(jogador)) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Jogo não encontrado ou não pertence ao jogador indicado.").build();
+        }
+
+        jogo.delete();
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+    
+    
+    
+    @DELETE
+    @Path("/{codigoJogador}/jogos")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response deleteAllJogosFromJogador(@PathParam("codigoJogador") Integer codigoJogador) {
+        Jogador jogador = Jogador.findById(codigoJogador);
+        if (jogador == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Jogador não encontrado.").build();
+        }
+
+        // Supondo que exista uma relação entre Jogador e Jogo, onde Jogador.jogos é uma coleção de jogos.
+        jogador.jogos.forEach(jogo -> jogo.delete());
+
+        // Confirme se você quer fazer commit imediato da transação ou se a mesma será gerenciada de outra forma.
+        // Se necessário, chame entityManager.flush() para garantir que as operações de delete sejam executadas imediatamente.
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    } 
+     
+    
+    
+    
+    
 }
